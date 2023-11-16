ce0da6f833d104cd949e072ab572a814f5e29c7d
Crowdbotics <team@crowdbotics.com>
2023-11-16 23:54:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hall-streaming-44633",
+  url: "https://hall-streaming-44633.botics.co",
+  api: "https://hall-streaming-44633.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
